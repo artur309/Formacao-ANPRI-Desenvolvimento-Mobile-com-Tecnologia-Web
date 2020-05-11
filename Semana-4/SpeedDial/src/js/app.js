5dc9976cd7789460949fd878a4150756a075d422
Artur Quaresma <artursquaresma@gmail.com>
2020-05-11 18:41:32 +0100

    SpeedDial visuals

--- Semana-4/SpeedDial/src/js/app.js
+++ Semana-4/SpeedDial/src/js/app.js
@@ -7,6 +7,7 @@ import '../css/framework7-custom.less';
 // Import Icons and App Custom Styles
 import '../css/icons.css';
 import '../css/app.less';
+import '../css/styles.css';
 // Import Cordova APIs
 import cordovaApp from './cordova-app.js';
 // Import Routes
@@ -16,56 +17,57 @@ import routes from './routes.js';
 import App from '../app.f7.html';
 
 var app = new Framework7({
-  root: '#app', // App root element
-  component: App, // App main component
-  id: 'io.framework7.myapp', // App bundle ID
-  name: 'Speed Dial', // App name
-  theme: 'auto', // Automatic theme detection
+	root: '#app', // App root element
+	component: App, // App main component
+	id: 'io.framework7.myapp', // App bundle ID
+	name: 'Speed Dial', // App name
+	theme: 'auto', // Automatic theme detection
 
+	// App routes
+	routes: routes,
 
-
-  // App routes
-  routes: routes,
-
-  // Register service worker
-  serviceWorker: Framework7.device.cordova ? {} : {
-    path: '/service-worker.js',
-  },
-  // Input settings
-  input: {
-    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
-    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
-  },
-  // Cordova Statusbar settings
-  statusbar: {
-    iosOverlaysWebView: true,
-    androidOverlaysWebView: false,
-  },
-  on: {
-    init: function () {
-      var f7 = this;
-      if (f7.device.cordova) {
-        // Init cordova APIs (see cordova-app.js)
-        cordovaApp.init(f7);
-      }
-    },
-  },
+	// Register service worker
+	serviceWorker: Framework7.device.cordova ? {} : {
+		path: '/service-worker.js',
+	},
+	// Input settings
+	input: {
+		scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+		scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
+	},
+	// Cordova Statusbar settings
+	statusbar: {
+		iosOverlaysWebView: true,
+		androidOverlaysWebView: false,
+	},
+	on: {
+		init: function () {
+			var f7 = this;
+			if (f7.device.cordova) {
+				// Init cordova APIs (see cordova-app.js)
+				cordovaApp.init(f7);
+			}
+		},
+	},
 });
 
-
 //evento init da página contatos
-$$(document).on('page:init', '.page[data-name="contatos"]', function (e) {
-  //verificar que a página foi carregada
-  console.log('contatos');
-  // Indicar qual o template a utilizar
-  var template = $$('#template-aluno').html();
-  // Compilar e renderizar o template
-  var compiledTemplate = Template7.compile(template);
-  // Obter os dados do ficheiro JSON
-  app.request.json('alunos.json', function (json) {
-    // Verificar que os dados foram lidos
-    console.log(json);
-    // Inserir os dados no template
-    $$('#result-wrap-aluno').html(compiledTemplate(json))
-  });
-})
+$('#btn').on('click', function(e) {
+	alert("Hello! I am an alert box!!");
+	console.log('test');
+	$('.contatosLista').toggleClass("visivel invisivel");
+});
+	/*//verificar que a página foi carregada
+	console.log('contatos');
+	// Indicar qual o template a utilizar
+	var template = $$('#template-aluno').html();
+	// Compilar e renderizar o template
+	var compiledTemplate = Template7.compile(template);
+	// Obter os dados do ficheiro JSON
+	app.request.json('alunos.json', function (json) {
+		// Verificar que os dados foram lidos
+		console.log(json);
+		// Inserir os dados no template
+		$$('#result-wrap-aluno').html(compiledTemplate(json))
+	});
+	*/
